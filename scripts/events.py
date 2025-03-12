6a3055e45d3d0f9673bffb8177c82197bbc79ff5
Jay Chia <17691182+jaychia@users.noreply.github.com>
2025-03-12 11:51:40 -0700

    Functional pubsub

--- scripts/events.py
+++ scripts/events.py
@@ -1,10 +1,40 @@
 import argparse
+import os
+import redis
 
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path=".env.dev")
+
+
+REDIS_ADDR = os.environ["REDIS_EVENTS_PUBSUB_ADDR"]
+REDIS_TOPIC_NAME = "tweets"
 
 
 def make_tweet(author: str, content: str):
     print(f"Making tweet for @{author}: {content}")
+    r = redis.Redis.from_url(f"redis://{REDIS_ADDR}")
+    tweet_data = {
+        "author": author,
+        "content": content
+    }
+    result = r.publish(REDIS_TOPIC_NAME, str(tweet_data))
+    print(f"Published to Redis topic `{REDIS_TOPIC_NAME}` with result: {result}")
 
+
+def view_tweet():
+    r = redis.Redis.from_url(f"redis://{REDIS_ADDR}")
+    pubsub = r.pubsub()
+    pubsub.subscribe(REDIS_TOPIC_NAME)
+    
+    print("Listening for tweets...")
+    for message in pubsub.listen():
+        if message["type"] == "message":
+            print(f"Received tweet: {message['data'].decode('utf-8')}")
+            break  # Exit after receiving one message
+    
+    pubsub.unsubscribe()
 
 
 if __name__ == "__main__":
@@ -15,6 +45,10 @@ if __name__ == "__main__":
     make_tweet_parser.add_argument("--author", required=True, help="Author name")
     make_tweet_parser.add_argument("content", help="Text content of the tweet")
 
+    make_tweet_parser = subparsers.add_parser("view-tweet", help="View tweets on event stream") 
+
     args = parser.parse_args()
     if args.command == "make-tweet":
         make_tweet(args.author, args.content)
+    elif args.command == "view-tweet":
+        view_tweet()
